baaa1e18b06c038af75c5a8ef9a1005ded6e34b9
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2017-09-23 12:13:55 -0500

    imported clock font, displayed time now passed as a prop

--- clock/src/components/clock.js
+++ clock/src/components/clock.js
@@ -3,10 +3,20 @@ import { Display } from './display'
 import { Buttons } from './buttons'
 
 export class Clock extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			hours: 12,
+			mins: '00',
+			orientation: 'am'
+		}
+	}	
+
 	render() {
 		return (
 			<div className="clock">
-				<Display/>
+				<Display mins={this.state.mins} hours={this.state.hours} orientation={this.state.orientation}/>
 				<Buttons/>
 			</div>
 		)
